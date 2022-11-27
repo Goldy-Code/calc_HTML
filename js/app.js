b3f46b0942a73db9366d93000a05673978f51e2e
Goldy-Code <113247980+Goldy-Code@users.noreply.github.com>
2022-11-27 11:21:49 +0500

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -7,16 +7,17 @@
 // document.getElementById("calcBtn").addEventListener("click", () => {});
 
 document.getElementById('calcBtn').addEventListener('click', () => {
-
+	const inputA = document.getElementById('inputA').value
+	const inputB = document.getElementById('inputB').value
+	const selectOp = document.getElementById('selectOp').value
 	let calculations = (a, b, op) => (
 		op === '/' ? a / b :
 			op === '*' ? a * b :
 				op === '+' ? +a + +b :
 					op === '-' ? a - b : 'write correctly'
 	)
-	let result = calculations(document.getElementById('inputA').value,
-		document.getElementById('inputB').value,
-		document.getElementById('selectOp').value);
+	let result = calculations(inputA, inputB, selectOp);
+	
 	document.getElementById('output').innerHTML = result
 
 	document.getElementById('inputA').value = ''
